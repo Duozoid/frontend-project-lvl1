3171630b7dc4f50ea4753e8d1c906389f275f585
Zoya <duozoid@gmail.com>
2022-06-06 11:23:51 +0300

    Ошибка в l;cjyt&

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
-
-
 console.log("Welcome to the Brain Games!"); 
+
+import {userName} from 'C:/Users/Зоя/frontend-project-lvl1/src/cli.js';
+
+console.log(`Hello, ${userName}!`);
+
